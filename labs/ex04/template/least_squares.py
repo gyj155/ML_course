d9af98dee6698eb6076888ac2c70808e3b30460e
gyj155 <gyj123@sjtu.edu.cn>
2024-10-08 23:06:13 +0200

    10.8

--- labs/ex04/template/least_squares.py
+++ labs/ex04/template/least_squares.py
@@ -7,6 +7,7 @@ Least Square
 import numpy as np
 
 
+from costs import compute_loss
 def least_squares(y, tx):
     """Calculate the least squares solution.
        returns mse, and optimal weights.
@@ -22,9 +23,6 @@ def least_squares(y, tx):
     >>> least_squares(np.array([0.1,0.2]), np.array([[2.3, 3.2], [1., 0.1]]))
     (array([ 0.21212121, -0.12121212]), 8.666684749742561e-33)
     """
-    # ***************************************************
-    # COPY YOUR CODE FROM EX03 HERE
-    # least squares: TODO
-    # returns optimal weights, MSE
-    # ***************************************************
-    raise NotImplementedError
+    w = np.linalg.inv(tx.T @ tx) @ tx.T @ y
+    mse = compute_loss(y, tx, w)
+    return w, mse
